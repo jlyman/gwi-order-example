93e125e4257372f3d721d5ca503135e8580748f8
Joshua Lyman <jlyman.work@gmail.com>
2017-05-16 15:50:17 -0500

    Initial clean commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Customer Profiles and Orders System</h1>
+    );
+  }
+}
+
+export default App;
