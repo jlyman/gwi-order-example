4d3cbd9286eca6399cfc71dba6e8ec553d251bac
Joshua Lyman <jlyman.work@gmail.com>
2017-05-17 06:05:39 -0500

    Place basic Redux file structure in place

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import allReducers from './redux/reducers';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
+let store = createStore(
+	allReducers,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <App />,
+	<Provider store={store}>
+  	<App />
+  </Provider>,
   document.getElementById('root')
 );
