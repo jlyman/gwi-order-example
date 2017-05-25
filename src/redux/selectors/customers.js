8802db1f24b512022901b4abc447268507d8753f
Joshua Lyman <jlyman.work@gmail.com>
2017-05-25 05:20:11 -0500

    Miscellaneous updates

---
+++ src/redux/selectors/customers.js
@@ -0,0 +1,6 @@
+export const customersById = (state) => state.customers.customersById;
+// export const currentlySelectedCustomer = (state) => state.customers.currentlySelected;
+
+// export const currentCustomer = (state) => {
+// 	return customersById(state)[currentlySelectedCustomer(state)];
+// }
